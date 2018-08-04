9a92764d20158e82978832c229311b09541ceb0f
huhq <huhq@itdayang.com>
2018-08-04 21:26:06 +0800

    [Feature]: Update config

--- src/im-config.js
+++ src/im-config.js
@@ -1,9 +1,18 @@
+const userType = new Map([
+    ['user', 'u'],
+    ['group', 'g'],
+    ['system', 's']
+]);
+
+const defaultConfig = {
+    msgSeq: '^-^',
+    url: 'ws://localhost:14001',
+    userType
+};
+
 class ImConfig {
     constructor(config = {}) {
-        this.defaultConfig = {
-            msgSeq: '^-^',
-            url: 'wss://****'
-        };
+        this.defaultConfig = defaultConfig;
         this.config = Object.assign(this.defaultConfig, config);
     }
 
--- src/test.js
+++ src/test.js
@@ -1,15 +1,26 @@
-import imSocket from './im-websocket.js';
+import Im from './im-core.js';
 
 let init = () => {
-    let im = new imSocket({
-        url: 'wss://test.io:14002/im',
-        reconnect: true,
-        log: true
+    let IM = new Im({
+        showLog: true
     });
 
-    im.on('error', (e) => {
-        console.log('get event:', e)
+    IM.util.connect();
+
+    IM.on('error', (e) => {
+        console.log('get event error:', e)
     });
+
+    IM.on('open', (e) => {
+        console.log('get event open:', e)
+        IM.send({
+            i: 1,
+            t: 1,
+            r: ['u1'],
+            c: 'Hola'
+        })
+    });
+
 };
 
 init();
